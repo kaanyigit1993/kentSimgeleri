6c0683647b4f59d4fcbe5fb94704bf02340cf2f2
KAAN YİĞİT <>
2021-04-14 00:46:25 +0300

    eklendi

--- KentSimgeleri/ViewController.swift
+++ KentSimgeleri/ViewController.swift
@@ -7,13 +7,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDelegate , UITableViewDataSource{
+    
+    var landMarkNames = [String]()
+    var landMarkImages = [UIImage]()
+    var choisenLandmarkNames = ""
+    var choisenLandmarkImages = UIImage()
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        print("asd")
+       
+        landMarkNames.append("Colosseum")
+        landMarkNames.append("Great Wall")
+        landMarkNames.append("Kremlin")
+        landMarkNames.append("Stonehenge")
+        landMarkNames.append("Tac Mahal")
+        
+        
+        landMarkImages.append(UIImage(named: "colosseum")!)
+        landMarkImages.append(UIImage(named: "greatwall")!)
+        landMarkImages.append(UIImage(named: "kremlin")!)
+        landMarkImages.append(UIImage(named: "stonehenge")!)
+        landMarkImages.append(UIImage(named: "tacmahal")!)
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            landMarkNames.remove(at: indexPath.row)
+            landMarkImages.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.middle)
+        }
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return landMarkNames.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = landMarkNames[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        choisenLandmarkNames = landMarkNames[indexPath.row]
+        choisenLandmarkImages = landMarkImages[indexPath.row]
+        performSegue(withIdentifier: "toImageViewController", sender: nil)
+        
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toImageViewController" {
+            let destinationVC = segue.destination as! imageViewController
+            destinationVC.selectedLandmarkName = choisenLandmarkNames
+            destinationVC.selectedLandmarkImage = choisenLandmarkImages
+        }
+    }
+    
 }
 
--- KentSimgeleri/imageViewController.swift
+++ KentSimgeleri/imageViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class imageViewController: UIViewController {
 
+    var selectedLandmarkImage = UIImage()
+    var selectedLandmarkName = ""
+    
+    @IBOutlet weak var landmarkLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        landmarkLabel.text = selectedLandmarkName
+        imageView.image = selectedLandmarkImage
+       
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
